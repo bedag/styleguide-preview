7ac764cfcd0376d0199c2579bf6a9c40324e13be
Christian Basler <christian.basler@bedag.ch>
2022-03-10 07:29:16 +0100

    Changes for github.io

--- update.sh
+++ update.sh
@@ -7,4 +7,5 @@ rm -rf docs
 mv ../bedag-angular-styleguide/frontend/dist/app docs
 
 sed -i "s#<base href=\"/\">#<base href=\"/styleguide-preview/\">#" docs/index.html
+cp docs/index.html docs/404.html
 
